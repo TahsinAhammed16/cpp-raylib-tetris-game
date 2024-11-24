76fa0c9b058b97773654e6e0d84f73587dd01576
TahsinAhammed16 <tahsin.ahammed16@gmail.com>
2024-11-24 21:59:49 +0600

    create a blank screen and the game loop

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+#include <raylib.h>
+
+int main() {
+    Color darkBlue = {44, 44, 127, 255};  
+    
+    InitWindow(300, 600, "Raylib Tetris");  
+    SetTargetFPS(60);  
+
+    while (!WindowShouldClose()) {  
+        BeginDrawing();
+        ClearBackground(darkBlue);  
+        EndDrawing();
+    }
+
+    CloseWindow();  
+    return 0;
+}
