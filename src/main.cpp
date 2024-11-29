61308e676866bd86c74d7d52cabf73791be0f8f1
TahsinAhammed16 <tahsin.ahammed16@gmail.com>
2024-11-29 13:53:36 +0600

    added auto-dropping for blocks with timer

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,18 @@
 #include <raylib.h>
 #include "game.h"
 
+double lastUpdateTime = 0;
+bool EventTriggered(double interval)
+{
+    double currentTime = GetTime();
+    if (currentTime - lastUpdateTime >= interval)
+    {
+        lastUpdateTime = currentTime;
+        return true;
+    }
+    return false;
+}
+
 int main()
 {
     Color darkBlue = {38, 70, 129, 255};
@@ -9,9 +21,15 @@ int main()
 
     Game game = Game();
 
-    while(!WindowShouldClose())
+    while (!WindowShouldClose())
     {
         game.HandleInput();
+
+        if (EventTriggered(0.3))
+        {
+            game.MoveBlockDown();
+        }
+
         BeginDrawing();
         ClearBackground(darkBlue);
         game.Draw();
